4a577e56194d1b6c7cc4446daf22882ac6502f6e
viswa <itsmorpheus@gmail.com>
2018-10-25 23:06:18 +0530

    Made my own version

---
+++ apps/01_hello_world/you_try/My hello tatti.py
@@ -0,0 +1,8 @@
+print('-----------------------------------------')
+print('------------Hello Everyone---------------')
+print('-----------------------------------------')
+print()
+
+Yes_No = input('Is your name tatti?')
+final_op = "Even though you typed " + Yes_No + ", you are indeed TATTI!"
+print(final_op)
